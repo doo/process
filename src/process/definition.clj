357739dd1637cea59e696b6b67d7a9448f49d926
Max Weber <max@doo.net>
2012-12-10 12:39:03 +0100

    A short notation to define the components of a process has been added (still alpha).

--- src/process/definition.clj
+++ src/process/definition.clj
@@ -2,7 +2,8 @@
   "A process definition is an ordinary map where the key is the name for the output
    of a component and the value is the component itself (a function for example, see fnc)."
   (:require [clojure.tools.namespace.dependency :as dep]
-            [clojure.set :as set]))
+            [clojure.set :as set]
+            [clojure.walk :as walk]))
 
 (defmacro fnc
   "A component function can be used as a component in a process definition. It
@@ -67,3 +68,52 @@
    is not satisfied by the available-outputs map."
   [fnc available-outputs]
   (set/difference (set (get-component-dependencies fnc)) (set (keys available-outputs))))
+
+(defn- extract-symbols
+  [form]
+  (let [symbols (atom [])
+        prepared-form
+        (walk/postwalk
+         (fn [x]
+           (if (symbol? x)
+             (swap! symbols conj x)
+             x)) form)]
+    @symbols))
+
+(defn unknown-symbols [refers symbols]
+  (let [refers (set refers)
+        symbols (set symbols)]
+    (set/difference symbols refers)))
+
+(defn- build-process-components
+  [refers bindings]
+  (->> bindings
+       (partition 2)
+       (map (fn [[output form]]
+              [(keyword output)
+               (let [symbols (extract-symbols form)
+                     dependencies (vec (unknown-symbols refers symbols))]
+                 (if (list? form)
+                   (list 'process.definition/fnc dependencies
+                         form)
+                   form))]))
+       (into {})))
+
+(defmacro process
+  "Alpha - subject to change.
+  Allows to define the components of a process with the
+  help of a short notation that is similar to the one of
+  clojure.core/let. Example:
+
+  (process [alpha 1
+            beta (+ alpha 1)])
+  will expand to:
+
+  {:alpha 1
+   :beta (process.definition/fnc [alpha] (+ alpha 1))}
+
+  Destructuring is not supported at the moment."
+  [bindings & {:keys [marker] :or {marker "$"}}]
+  (let [refers (concat (keys (ns-refers *ns*))
+                       (keys &env))]
+    (build-process-components refers bindings)))
